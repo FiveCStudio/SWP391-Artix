05d56f53ba58be3d7bcab8d11307a42f5a6a644f
Rous1141 <trieupmse172937@fpt.edu.vn>
2024-03-31 22:01:30 +0700

    FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,3 @@
-<<<<<<< Updated upstream
-
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import FrontPage from './Components/FrontPage.jsx';
-import Mod from './Components/Mods/Mod.jsx';
-import Users from './Components/Users/Users.jsx';
-import CreateAccount from './Components/Forms/CreateAccount.tsx';
-import Unauthorized from './ProtectedRoutes/Unauthorized.jsx';
-import ProtectedRoute from './ProtectedRoutes/ProtectedRoute.tsx';
-import RouterAdmin from './Components/Admin/RouterAdmin.jsx';
-function App() {
-  const role = sessionStorage.getItem('userRole')
-  console.log(role)
-  return (
-    <div className='App'>
-      
-        <Routes>
-          <Route path='/' element={<FrontPage/>}/>
-          <Route path='/unauthorized' element={<Unauthorized/>}/>
-          <Route element={<ProtectedRoute allowedRoles={['AD']}/>}>
-            <Route path="/admin/*" element={<RouterAdmin/>}></Route>
-          </Route>
-          <Route path="/createaccount" element={<CreateAccount/>}/>
-          <Route path="/characters/*" element={<Users/>}></Route>
-        </Routes>
-    </div>
-  );
-}
-
-export default App;
-=======
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import FrontPage from "./Components/FrontPage.jsx";
@@ -56,4 +24,3 @@ function App() {
 }
 
 export default App;
->>>>>>> Stashed changes
