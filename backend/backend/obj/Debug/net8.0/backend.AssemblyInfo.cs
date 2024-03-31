be2a467d7333f50cb5146c9a4fd72ffb9ce9d2b5
Rous1141 <trieupmse172937@fpt.edu.vn>
2024-03-31 22:05:40 +0700

    BE

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67735fd78891a172652c5ce8ee4ca4f85dc54aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d56f53ba58be3d7bcab8d11307a42f5a6a644f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
