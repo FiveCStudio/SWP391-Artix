9f50882bd073741161fcb8f4f3ea9ed8d0e8c0e4
TAT610 <tuyettase170234@fpt.edu.vn>
2024-04-03 12:48:41 +0700

    Artshop

--- frontend/src/Components/Users/ArtShop.jsx
+++ frontend/src/Components/Users/ArtShop.jsx
@@ -1,1 +1,42 @@
-// CODE HERE!
+import { Backdrop, Box, Button, Card, CardActions, CardContent, CircularProgress, IconButton, Pagination, Stack, Typography } from '@mui/material'
+import React, { useContext, useEffect, useState } from 'react'
+import { useAuth } from '../AuthenContext.tsx'
+import { getArtWithStatus } from '../../API/ArtShop/ArtShopServices.js';
+import { Discount, Download, Headset, More, ShareLocation, Shop } from '@mui/icons-material';
+import { pink } from '@mui/material/colors';
+import "../../css/ArtShop.css";
+import html2canvas from 'html2canvas';
+import { Link } from 'react-router-dom';
+import ArtShopDialog from './ArtShopDialog.jsx';
+function ArtShop() {
+    const auth = JSON.parse(sessionStorage.getItem("auth"));
+    const [dataState, setDataSate] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [openDowload, setOpenDowload] = useState(false);
+    const handleClose = () => {
+        setOpen(false);
+        setOpenDowload(false);
+        setDataSate((pre) => ({
+            ...pre,
+            idDowLoad: null
+        }))
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const search = async () => {
+        try {
+            handleOpen();
+            const data = await getArtWithStatus(auth?.creatorID, dataState?.currentPage);
+            setDataSate((pre) => ({
+                ...pre,
+                listItem: data?.data?.artworkViewModels,
+                totalPages: data?.data?.totalPages,
+            }))
+        } catch (error) {
+
+        } finally {
+            handleClose();
+        }
+    }
