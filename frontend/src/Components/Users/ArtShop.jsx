79234ae41733f6137ddb58720e7f5211494ee29b
TAT610 <tuyettase170234@fpt.edu.vn>
2024-04-03 13:02:10 +0700

    1

--- frontend/src/Components/Users/ArtShop.jsx
+++ frontend/src/Components/Users/ArtShop.jsx
@@ -1,11 +1,2 @@
-import { Backdrop, Box, Button, Card, CardActions, CardContent, CircularProgress, IconButton, Pagination, Stack, Typography } from '@mui/material'
-import React, { useContext, useEffect, useState } from 'react'
-import { useAuth } from '../AuthenContext.tsx'
-import { getArtWithStatus } from '../../API/ArtShop/ArtShopServices.js';
-import { Discount, Download, Headset, More, ShareLocation, Shop } from '@mui/icons-material';
-import { pink } from '@mui/material/colors';
-import "../../css/ArtShop.css";
-import html2canvas from 'html2canvas';
-import { Link } from 'react-router-dom';
-import ArtShopDialog from './ArtShopDialog.jsx';
+
 
