9c5b900983fe4646632b8d912bfdb0d55e348cc1
TAT610 <tuyettase170234@fpt.edu.vn>
2024-04-03 13:03:44 +0700

    1

--- frontend/src/Components/Users/ArtShop.jsx
+++ frontend/src/Components/Users/ArtShop.jsx
@@ -9,3 +9,36 @@ import html2canvas from 'html2canvas';
 import { Link } from 'react-router-dom';
 import ArtShopDialog from './ArtShopDialog.jsx';
 
+function ArtShop() {
+    const auth = JSON.parse(sessionStorage.getItem("auth"));
+    const [dataState, setDataSate] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [openDowload, setOpenDowload] = useState(false);
+    const handleClose = () => {
+        setOpen(false);
+        setOpenDowload(false);
+        setDataSate((pre) => ({
+            ...pre,
+            idDowLoad: null
+        }))
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const search = async () => {
+        try {
+            handleOpen();
+            const data = await getArtWithStatus(auth?.creatorID, dataState?.currentPage);
+            setDataSate((pre) => ({
+                ...pre,
+                listItem: data?.data?.artworkViewModels,
+                totalPages: data?.data?.totalPages,
+            }))
+        } catch (error) {
+
+        } finally {
+            handleClose();
+        }
+    }
+    
