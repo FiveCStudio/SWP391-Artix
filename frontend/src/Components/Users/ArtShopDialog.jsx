e08ed19083b9fb9dca5b565d5ce8583e4dc87b10
Rous1141 <trieupmse172937@fpt.edu.vn>
2024-04-03 12:48:32 +0700

    Fix FE

--- frontend/src/Components/Users/ArtShopDialog.jsx
+++ frontend/src/Components/Users/ArtShopDialog.jsx
@@ -1,1 +1,32 @@
-/// Code Dialog Here!
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function ArtShopDialog(props) {
+    let {
+        open,
+        handleYesClick,
+        handleClose
+    } = props;
+
+    return (
+        <React.Fragment>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Confirm dowload?"}
+                </DialogTitle>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleYesClick}>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
+}
