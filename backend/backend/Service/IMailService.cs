710a67b4c079009892287a294a3f82ba331a1061
thucnguyen1234 <127202577+thucnguyen12356666@users.noreply.github.com>
2024-04-04 14:16:32 +0700

    a

--- backend/backend/Service/IMailService.cs
+++ backend/backend/Service/IMailService.cs
@@ -1,6 +1,14 @@
-﻿namespace backend.Service
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using backend.Entities;
+namespace backend.Service
 {
-    public class IMailService
+    public interface IMailService
     {
+        Task SendEmailAsync(MailRequest mailRequest);
+        Task SendWelcomeEmailAsync(WelcomeRequest request);
     }
 }
--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aebbb8c6908f63b4fcba881339a5d8ade76c8963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4e883c8d5e6f929f669bc6c76586cd54a504a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
