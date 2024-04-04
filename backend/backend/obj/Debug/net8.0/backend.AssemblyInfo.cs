a674792440232f4cded02e12e0a6ba78d42f74bb
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-04-04 14:04:52 +0700

    Add DTO
    PaymentRespone - VnPayLibrary

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4f6f786dc384ada9e4d85702b8ae07584fbe03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e7ae236194998c5b76feda1ab2fba931976efdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
