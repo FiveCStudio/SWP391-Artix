7b6c69b185e8e822c97c342fec4771111f93227d
TAT610 <tuyettase170234@fpt.edu.vn>
2024-04-05 00:28:30 +0700

    dl

--- backend/backend/Entities/ArtworksResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace backend.Entities
-{
-    public class ArtworksResponse
-    {
-        public int TotalPages { get; set; }
-        public List<ArtworkViewModel> ArtworkViewModels { get; set; }
-    }
-}
