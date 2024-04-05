b6e4e74c1f643760d333205078e6cbe82a52fdb8
Rous1141 <trieupmse172937@fpt.edu.vn>
2024-04-05 13:33:05 +0700

    Revert "BE"

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+707ac7b27f2e29b35bf0fd8faa1a68cda872eb7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb306f7cf3af6c2968bbb2755bd54ce18429af8")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
