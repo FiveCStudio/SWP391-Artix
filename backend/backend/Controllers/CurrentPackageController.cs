e623c69a83a4c3150b8d6c8109541cd156b2cbb5
thucnguyen1234 <127202577+thucnguyen12356666@users.noreply.github.com>
2024-04-05 10:11:25 +0700

    a

---
+++ backend/backend/Controllers/CurrentPackageController.cs
@@ -0,0 +1,46 @@
+﻿using backend.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrentPackageController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CurrentPackageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CurrentPackage
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CurrentPackage>>> GetCurrentPackages()
+        {
+            return await _context.CurrentPackage.ToListAsync();
+        }
+
+        // GET: api/CurrentPackage/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CurrentPackage>> GetCurrentPackage(int id)
+        {
+            var currentPackage = await _context.CurrentPackage.FindAsync(id);
+
+            if (currentPackage == null)
+            {
+                return NotFound();
+            }
+
+            return currentPackage;
+        }
+
+
+
+
+    }
+}
